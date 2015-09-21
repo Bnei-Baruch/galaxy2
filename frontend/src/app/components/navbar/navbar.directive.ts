e11deebed8c02462a17899dbe82a581bf1e2bb51
Kolman Vornovitsky <kolmanv@gmail.com>
2015-09-20 22:42:07 -0400

    Initial commit

---
+++ frontend/src/app/components/navbar/navbar.directive.ts
@@ -0,0 +1,28 @@
+module frontend {
+  'use strict';
+
+  /** @ngInject */
+  export function acmeNavbar(): ng.IDirective {
+
+    return {
+      restrict: 'E',
+      scope: {
+        creationDate: '='
+      },
+      templateUrl: 'app/components/navbar/navbar.html',
+      controller: NavbarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+  }
+
+  /** @ngInject */
+  class NavbarController {
+    public relativeDate: string;
+
+    constructor(moment: moment.MomentStatic) {
+      this.relativeDate = moment(1442660917121).fromNow();
+    }
+  }
+}
