e542a906aef6d9690f0563fb57b1135b9f6e2ad2
Edo Shor <edo.shor@pipl.com>
2015-10-26 04:50:28 +0200

    Backend - First commit of new project skeleton. MongoDB + Devise + RailsAdmin + Bootstrap

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
