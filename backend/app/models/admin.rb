c5e027988720b592cac5ec27ded543f0e2f7a07c
Edo Shor <edo.shor@pipl.com>
2015-10-30 05:19:39 +0200

    Backend - /users/breakdown api endpoint

--- backend/app/models/admin.rb
+++ backend/app/models/admin.rb
@@ -1,5 +1,6 @@
 class Admin
   include Mongoid::Document
+  include Mongoid::Timestamps
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -33,4 +34,5 @@ class Admin
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
+
 end
