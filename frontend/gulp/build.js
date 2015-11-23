926713bbce4be7689e96e0d4acaed38ca1e325b4
kolmanv <kolmanv@gmail.com>
2015-11-22 22:36:10 -0500

    Config task dependency added to gulp tasks

--- frontend/gulp/build.js
+++ frontend/gulp/build.js
@@ -94,4 +94,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/partials'), path.join(conf.paths.tmp, '/serve')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['config', 'html', 'fonts', 'other']);
