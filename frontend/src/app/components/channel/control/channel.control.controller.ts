a0789894637c310f104b667a296c1e4be617fdf4
Konstantin Alexandrov <iwuvjhdva@gmail.com>
2015-12-30 16:20:16 +0000

    Control channel autocomplete search implemented

---
+++ frontend/src/app/components/channel/control/channel.control.controller.ts
@@ -0,0 +1,41 @@
+import { ChannelController } from '../channel.controller';
+import { IUser } from '../../../shidur/shidur.service';
+
+/** @ngInject */
+export class ControlChannelController extends ChannelController {
+  users: IUser[] = [];
+  usersBreakdown: { [channel: string]: IUser[]; };
+  searchText: string;
+  selectedUser: IUser;
+
+  pickUser(user: IUser): void {
+    if (user !== undefined) {
+      this.users.unshift(user);
+    }
+  }
+
+  querySearch(searchText: string): IUser[] {
+    var users = [];
+    console.log('query search', searchText);
+
+    searchText = searchText || '';
+    var lowercaseText = searchText.trim().toLowerCase();
+
+    var pickedLogins = this.users.map((user: IUser) => { return user.login; });
+
+    for (var channel in this.usersBreakdown) {
+      if (this.usersBreakdown.hasOwnProperty(channel) && channel !== 'hidden') {
+        var channelUsers = this.usersBreakdown[channel].filter((user: IUser) => {
+          if (pickedLogins.indexOf(user.login) !== -1) {
+            return false;
+          }
+          var titleLogin = (user.login + user.title).toLowerCase();
+          return titleLogin.indexOf(lowercaseText) !== -1;
+        });
+        users = users.concat(channelUsers);
+      }
+    }
+
+    return users;
+  }
+}
--- frontend/src/app/components/channel/control/channel.control.directive.ts
+++ frontend/src/app/components/channel/control/channel.control.directive.ts
@@ -1,4 +1,5 @@
-// import { ChannelController, IChannelScope } from './channel.controller';
+import { IChannelScope } from '../channel.controller';
+import { ControlChannelController } from './channel.control.controller';
 
 /** @ngInject */
 export function channelControlWidget(): ng.IDirective {
@@ -10,15 +11,15 @@ export function channelControlWidget(): ng.IDirective {
       usersBreakdown: '=',
       hotkey: '@'
     },
-    // link: ($scope: IChannelScope, el, attrs, ctrl) => {
-    //   $scope.selfElement = el;
-    //   $scope.programElement = el.find('.program').get(0);
-    //   $scope.previewElement = el.find('.preview').get(0);
-    // },
+    link: ($scope: IChannelScope, el, attrs, ctrl) => {
+      $scope.selfElement = el;
+      $scope.programElement = el.find('.program').get(0);
+      $scope.previewElement = el.find('.preview').get(0);
+    },
     templateUrl: 'app/components/channel/control/channel.control.html',
-    // controller: ChannelController,
-    // controllerAs: 'vm',
-    // bindToController: true
+    controller: ControlChannelController,
+    controllerAs: 'vm',
+    bindToController: true
   };
 
 }
