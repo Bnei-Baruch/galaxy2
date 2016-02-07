069eed2c1dd4b35d0e416378421c409e2b08eff0
Konstantin Alexandrov <iwuvjhdva@gmail.com>
2016-02-07 15:19:27 +0000

    Sync disabled for debug server

--- frontend/gulp/server.js
+++ frontend/gulp/server.js
@@ -38,6 +38,7 @@ function browserSyncInit(baseDir, browser) {
   browserSync.instance = browserSync.init({
     startPath: '/',
     server: server,
+    ghostMode: false,
     https: true,
     browser: browser
   });
