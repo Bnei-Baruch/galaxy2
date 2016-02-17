4ed1c80ddf4d9584559006753c2766dd62da5b50
davgur <davgur.ru@gmail.com>
2016-02-17 03:12:59 +0000

    return improve css changes

--- frontend/src/app/components/navbar/navbar.directive.ts
+++ frontend/src/app/components/navbar/navbar.directive.ts
@@ -19,11 +19,13 @@ export class NavbarController {
   $rootScope: IGalaxyScope;
   auth: AuthService;
   toastr: any;
+  $mdSidenav: angular.material.ISidenavService;
 
-  constructor ($rootScope: IGalaxyScope, authService: AuthService, toastr: any) {
+  constructor($rootScope: IGalaxyScope, authService: AuthService, $mdSidenav: angular.material.ISidenavService, toastr: any) {
     this.$rootScope = $rootScope;
     this.auth = authService;
     this.toastr = toastr;
+    this.$mdSidenav = $mdSidenav;
   }
 
   openMenu($mdOpenMenu, e) {
