73a57cdf84c0a7fcfac4917b28222aa285f491c0
Konstantin Alexandrov <iwuvjhdva@gmail.com>
2016-02-18 13:13:45 +0000

    Faye handlers removed

--- backend/app/controllers/pub_sub_controller.rb
+++ backend/app/controllers/pub_sub_controller.rb
@@ -1,17 +1,6 @@
 # API documentation for faye-rails: https://github.com/jamesotron/faye-rails
 
 class PubSubController < FayeRails::Controller
-  # FayeRails monitoring doesn't handle disconnects, handling with Faye server instead
-  FayeRails::server.on(:disconnect) do |client_id|
-      puts "Client #{client_id} disconnected from server."
-  end
-
-  channel '/auth' do
-    subscribe do
-      puts "Received on channel #{channel}: #{message.inspect}"
-    end
-    monitor :publish do
-      puts "Client #{client_id} published #{data.inspect} to #{channel}."
-    end
+  channel '/' do
   end
 end
