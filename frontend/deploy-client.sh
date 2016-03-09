46ac3dc43edecd6e2022d90d235b7c5d32bbc04b
Edo Shor <edo.shor@pipl.com>
2016-03-09 05:53:40 +0200

    Deployment script for frontend

---
+++ frontend/deploy-client.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+echo "-----> Deploying galaxy2 frontend" && (
+  cd /sites/galaxy.kbb1.com/www/ &&
+  echo "pulling changes from git" &&
+  git pull &&
+
+  cd frontend &&
+
+  echo "tsd install" &&
+  tsd install &&
+
+  echo "npm install" &&
+  npm install &&
+  npm ls --depth 0 &&
+
+  echo "bower install" &&
+  bower install &&
+  bower list &&
+
+  echo "Patching config.json" &&
+  sha1="$(git rev-parse HEAD)" &&
+  echo "SHA1=$sha1" &&
+  sed -i "s/git-sha1-deployed-should-be-here/$sha1/g" config.json &&
+
+  echo "gulp config" &&
+  gulp config &&
+
+  echo "gulp build" &&
+  gulp build &&
+
+  echo "" &&
+  echo "-----> Done."
+) || (
+  echo "! ERROR: Deployment failed."
+)
