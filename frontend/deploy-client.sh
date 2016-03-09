c2bd88993c856cec391818b5b1f4215f00c7c30e
Edo Shor <edo.shor@pipl.com>
2016-03-09 06:36:38 +0200

    Deployment script - rewind git sha1 after build

--- frontend/deploy-client.sh
+++ frontend/deploy-client.sh
@@ -28,6 +28,9 @@ echo "-----> Deploying galaxy2 frontend" && (
   echo "gulp build" &&
   gulp build &&
 
+  echo "Rewind patches to config.json" &&
+  sed -i "s/$sha1/git-sha1-deployed-should-be-here/g" config.json &&
+
   echo "" &&
   echo "-----> Done."
 ) || (
