6579d2abcec1289f2a6a6cf011dbf2221937679b
Kolya Kavod <creature@bk.ru>
2016-04-02 14:57:58 +0000

    Umcomment some tests.

--- frontend/e2e/janus.spec.js
+++ frontend/e2e/janus.spec.js
@@ -16,7 +16,7 @@ describe('Janus video room', function () {
     browser.ignoreSynchronization = false;
   });
 
-  xit('should display user video when the user logged in before shidur page loaded', function() {
+  it('should display user video when the user logged in before shidur page loaded', function() {
     browser.get('/#/user');
     utils.login();
     utils.openNewWindow("/index.html");
@@ -24,7 +24,7 @@ describe('Janus video room', function () {
     utils.waitForVideo('.preview');
   });
 
-  xit('should display user video when the user logged in after shidur page loaded', function() {
+  it('should display user video when the user logged in after shidur page loaded', function() {
     browser.get('/index.html');
     utils.login();
     utils.openNewWindow("/#/user");
@@ -32,7 +32,7 @@ describe('Janus video room', function () {
     utils.waitForVideo('.preview');
   });
 
-  xit('should resume getting dynamic video when user reloads the page', function() {
+  it('should resume getting dynamic video when user reloads the page', function() {
     // Open user
     browser.get('/#/user');
     utils.login();
@@ -49,7 +49,7 @@ describe('Janus video room', function () {
     utils.waitForVideo('.preview');
   });
 
-  xit('should resume getting user video when shidur reloads the page', function() {
+  it('should resume getting user video when shidur reloads the page', function() {
     // Open user
     browser.get('/#/user');
     utils.login();
