fc21b29ed1dbb8f49ba89ed495f924b12a340307
davgur <davgur.ru@gmail.com>
2016-06-13 00:55:48 +0000

    remove .js

--- frontend/src/app/components/pubSub/pubSub.service.js
+++
@@ -1,8 +0,0 @@
-/** @ngInject */
-var PubSubService = (function () {
-    function PubSubService(config) {
-        this.client = new Faye.Client(config.backendUri + '/pubsub');
-    }
-    return PubSubService;
-})();
-exports.PubSubService = PubSubService;
