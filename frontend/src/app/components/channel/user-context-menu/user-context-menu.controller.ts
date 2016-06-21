028baef97d1bdb96d47c8794b9c4ab2597839dd3
Edo Shor <edo.shor@pipl.com>
2016-06-21 05:32:45 +0300

    Reload specific user

--- frontend/src/app/components/channel/user-context-menu/user-context-menu.controller.ts
+++ frontend/src/app/components/channel/user-context-menu/user-context-menu.controller.ts
@@ -1,19 +1,35 @@
 import { PubSubService } from '../../pubSub/pubSub.service';
-import { JanusVideoRoomService } from '../../janus/janusVideoRoom.service';
+import {IUser} from "../../auth/auth.service";
 
 /* @ngInject */
 export class UserContextMenuController {
   constructor(private pubSub: PubSubService,
-              private videoRoom: JanusVideoRoomService,
+              private $log: ng.ILogService,
               private toastr: any) {
   }
 
   reloadChannelUsers(channelName: string) {
+    this.$log.debug('Reload channel', channelName);
     this.pubSub.client.publish('/admin', {
       message: 'reload',
       channel: channelName
     }).then(() => {
-      this.toastr.info(`Reload command has been sent to the channel users.`);
+      this.toastr.info(`Reloading users in ${channelName} channel`);
+    }, (error: any) => {
+      this.$log.error('Error reloading channel users', channelName, error);
+      this.toastr.error(`Error reloading users in ${channelName} channel`);
+    });
+  }
+  
+  reloadUser(user: IUser) {
+    this.$log.debug('Reload user', user.login);
+    this.pubSub.client.publish('/users/' + user.login, {
+      message: 'reload',
+    }).then(() => {
+      this.toastr.info(`Reloading ${user.title}`);
+    }, (error: any) => {
+      this.$log.error('Error reloading user', user.login, error);
+      this.toastr.error(`Error reloading ${user.title}`);
     });
   }
 }
